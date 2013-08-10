2ed09811d7dec2deee7adfe5e667ec4c07f10899
zacharywegrzyniak <zacharywegrzyniak@u.boisestate.edu>
2013-08-10 10:43:20 -0600

    running again

--- clusterer.py
+++ clusterer.py
@@ -1,91 +1,101 @@
 import csv
-from nexus import Cluster
+from nexus import Cluster, Geopoint
 from numpy  import *
-import scipy.cluster.vq as clusterer
+import scipy.cluster.vq as kmean
 
-#Plots centroids locations on a static Google Map
-def center_plotter(centroidList):
-    print("\nThe centers of the clusters are located at: \n")
-    colors = ["black","purple","blue","red","orange","yellow", "white"]
-    URL_prefix = "http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\"
-    number = 1
-    markers = ""
-    for centroid in centroidList:
-        if centroid.size > 1:
-            print(number, "-->", centroid.center.toString(), "and has", centroid.size, "customers around it.")
+class Clusterer:
+    '''Plots centroids locations on a static Google Map.'''
+    
+    def plot_center(centroidList):
+        print('\nThe centers of the clusters are located at: \n')
+        colors = ['black','purple','blue','red','orange','yellow', 'white']
+        URL_prefix = 'http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\'
+        number = 1
+        markers = ''
+        for centroid in centroidList:
+            if centroid.size > 1:
+                form = (number, centroid.center, centroid.size)
+                print('{} --> {} has {} points around it.'.format(*form))
+                if (len(URL_prefix +  markers) + 15 > 2000):
+                    print(''.join([URL_prefix, markers, '&sensor=false']))
+                    markers = ''
+                markers += '&markers=%7Ccolor:blue%7Clabel:{}%7C{},{}'.format(number, centroid.center.lat, centroid.center.lon)
+            number += 1
+
+        final_URL = '{}{}%7C&sensor=false'.format(URL_prefix, markers)
+        print('\nThe map of the cluster centers\n{}\n{}\n'.format(final_URL, '_'*37))
+
+    def plotter(raw_file, center):
+        if center.lat == 0 and center.lon == 0:
+            return 0
+        colors = ['black','purple','blue','red','orange','yellow', 'white']
+        URL_prefix = 'http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\'
+        color = colors[5]
+        markers = '&markers=%7Ccolor:green%7C{},{}'.format(center.lat, center.lon)
+
+        letter = ord('A')
+        for point in raw_file:
+            if color != colors[int(point[4]) % (len(colors) - 1)]:
+                color = colors[int(point[4]) - 1]
+
             if (len(URL_prefix +  markers) + 15 > 2000):
-                print(URL_prefix + markers + "&sensor=false")
-                markers = ""
-            markers += "&markers=%7Ccolor:blue%7Clabel:" + str(number) + "%7C" + str(centroid.center.lat) + "," + str(centroid.center.lon)
-        number += 1
+                print(URL_prefix + markers + '&sensor=false')
+                markers = ''
+            form = (color, chr(letter), point[2], point[3])
+            markers += '&markers=%7Ccolor:{}%7Clabel:{}%7C{},{}'.format(*form)
+            letter += 1
 
-    final_URL = URL_prefix + markers + "%7C&sensor=false"
-    print("\nThe map of the cluster centers\n", final_URL, "\n_____________________________________\n", sep = "")
+        final_URL = '{}{}%7C&sensor=false'.format(URL_prefix, markers)
+        print(final_URL, end='\n\n')
 
-def Plotter(list, center):
-    if (center.lat == 0 and center.lon == 0):
-        return 0
-    colors = ["black","purple","blue","red","orange","yellow", "white"]
-    URL_prefix = "http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\"
-    color = colors[5]
-    markers = "&markers=%7Ccolor:green%7C" + str(center.lat) + "," + str(center.lon)
-
-    letter = ord('A')
-    for person in list:
-        if color != colors[int(person[4]) % (len(colors) - 1)]:
-            color = colors[int(person[4]) - 1]
- #           markers += "&markers=%7Ccolor:" + color
-
-        if (len(URL_prefix +  markers) + 15 > 2000):
-            print(URL_prefix + markers + "&sensor=false")
-            markers = ""
-        markers += "&markers=%7Ccolor:" + color + "%7Clabel:" + chr(letter) + "%7C" + person[3] + "," + person[2]
-        letter += 1
-
-    final_URL = URL_prefix + markers + "%7C&sensor=false"
-    print(final_URL, "\n", sep = "")
-
-def scan_clustering(placement):
-    for i in range (0, len(placement)):
-        atLeastOne = False
-        for j in range (0, len(placement)):
-            if i != j and placement[i] == placement[j]:
-                atLeastOne = True
-        if atLeastOne == False:
-            return False
-    return True
+    def scan_clustering(placement):
+        for i in range (len(placement)):
+            atLeastOne = False
+            for j in range (len(placement)):
+                if i != j and placement[i] == placement[j]:
+                    atLeastOne = True
+            if atLeastOne == False:
+                return False
+        return True
 
 #parses the incoming csv
-list = []
+raw_file = []
 with open('cluster.csv') as csvfile:
-    infoReader = csv.reader(csvfile, delimiter=',', quotechar=' ')
-
+    infoReader = csv.reader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
     for row in infoReader:
         column = []
         for item in row:
             column.append(item.strip())
-        list.append(column)
+        raw_file.append(column)
 
-header = list.pop(0) #takes care of header
+header = raw_file.pop(0) #takes care of header
 
-#build 2-dimensional array for kmeans
 obs_vector = []
-for row in list:
-    try:
-        obs_vector.append([float(row[3]), float(row[2])])
-    except:
-        print("something went wrong...You should probably rerun the program.")
+for item in raw_file:
+    name = item[0]
+    address = item[1]
+    lat = item[2]
+    lon = item[3]
+    raw_file
+    if lat == '' or lon == '':
+        temp = Geopoint(name, address)
+        obs_vector.append([temp.lon, temp.lat])
+    else:
+        temp = Geopoint(name, address, lat, lon)
+
+        
+print(obs_vector)
 
 matrix = array(obs_vector)
 
-numberOfClusters = 0
+numberOfClusters = 2
 lastSolution = []
 for i in range (2, int(len(obs_vector)/2)):
-    cluster = clusterer.kmeans2(matrix, i, iter = 1000, minit = 'random')
-    if (scan_clustering(cluster[1])):
+    cluster = kmean.kmeans2(matrix, i, iter=1000, minit='random')
+    if Clusterer.scan_clustering(cluster[1]):
         numberOfClusters = i
         lastSolution = cluster
-        print("Clusters =", i, cluster[1])
+        print('Clusters =', i, cluster[1])
 
 center = []
 
@@ -93,33 +103,34 @@ for i in range (0, numberOfClusters):
     center.append(Cluster())
 
 codedList = []
-for i in range (0, len(list)):
+for i in range (len(raw_file)):
+    print(lastSolution)
     cluster = lastSolution[1][i]
-    list[i][6]= cluster
+    raw_file[i][4] = cluster
     clusterNode = center[cluster]
     theCenter = center[cluster].center
     if (theCenter.lat == 0 and theCenter.lon == 0):
-        theCenter.lat = lastSolution[0][cluster][0]
-        theCenter.lon = lastSolution[0][cluster][1]
+        theCenter.lat = lastSolution[0][cluster][1]
+        theCenter.lon = lastSolution[0][cluster][0]
 
     clusterNode.size += 1
-    clusterNode.points.append(list[i])
+    clusterNode.points.append(raw_file[i])
 
-center_plotter(center)
+Clusterer.plot_center(center)
 
 cluster_number = 1
 for centroid in center:
     if centroid.size > 1:
-        print("Cluster", cluster_number, " ", centroid.center.toString(), "size:", centroid.size)
-        print("A static map of the cluster")
-        Plotter(centroid.points,centroid.center)
-        i = ord("A")
-        pointsLined = "from: 1516 S Colorado Ave, Boise, ID"
+        form = (cluster_number, centroid.center, centroid.size)
+        print('Cluster {} centered at {} size: {}'.format(*form))
+        print('A static map of the cluster')
+        Clusterer.plotter(centroid.points,centroid.center)
+        i = ord('A')
+        pointsLined = 'from: Capitol Boise ID'
         for points in centroid.points:
-            print(chr(i), "is", points[0], points[1], "|", header[4], points[4], "|", header[5], points[5])
-            pointsLined += (" to: " + points[1])
+            print('{} is {}, {} | {} | {} |'.format(chr(i), points[0], points[1], header[4], points[4]))
+            pointsLined += (' to: ' + points[1])
             i += 1
-        pointsLined += " to: 1516 S Colorado Ave, Boise, ID"
-        print("\nPaste this into Google Maps for distance info:\n" + pointsLined)
-        print("___________________________________________\n")
+        pointsLined += ' to: Capitol Boise ID'
+        print('\nPaste this into Google Maps for distance info:\n{}\n{}'.format(pointsLined, '_'*43))
     cluster_number += 1
