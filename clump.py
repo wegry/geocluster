0d2c5f412b24e93671947e146d560adf6348831f
zach wegrzyniak <zwegrzyniak@gmail.com>
2014-01-15 15:25:18 -0700

    cleaning things up

--- clump.py
+++ clump.py
@@ -1,150 +1,169 @@
 import csv
 
-from nexus import Cluster, Geopoint
-import scipy.cluster.vq as kmean
 from numpy import *
+import scipy.cluster.vq as kmean
+
+from nexus import Cluster, Geopoint
+
+
 
 class Clump:
-	'''Plot centroid locations on a static Google Map.'''
-	def __init__(self):
-		self.clusters = []
-	
-	def plot_center(centroid_list):
-		print('\nThe centers of the clusters are located at: \n')
-		URL_prefix = 'http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\'
-		number = 1
-		markers = ''
-		for centroid in centroid_list:
-			if centroid.size > 1:
-				form = (number, centroid.center, centroid.size)
-				print('{} --> {} has {} points around it.'.format(*form))
-				if (len(URL_prefix +  markers) + 15 > 2000):
-					print(''.join([URL_prefix, markers, '&sensor=false']))
-					markers = ''
-				markers += '&markers=%7Ccolor:blue%7Clabel:{}%7C{},{}'.format(number, centroid.center.lat, centroid.center.lon)
-			number += 1
 
-		final_URL = '{}{}%7C&sensor=false'.format(URL_prefix, markers)
-		print('\nThe map of the cluster centers\n{}\n{}\n'.format(final_URL, '_'*37))
+    '''Plot centroid locations on a static Google Map.'''
 
-	def plotter(parsed_file, center):
-		if center.lat == 0 and center.lon == 0:
-			return 0
-		colors = ['black','purple','green','red','orange','yellow', 'white']
-		URL_prefix = 'http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\'
-		color = colors[5]
-		markers = '&markers=%7Ccolor:blue%7C{},{}'.format(center.lat, center.lon)
+    def __init__(self):
+        self.clusters = []
+        self.header = []
 
-		letter = ord('A')
-		for point in parsed_file:
-			if color != colors[int(point[4]) % (len(colors) - 1)]:
-				color = colors[int(point[4]) - 1]
+    def plot_centers(self, centroid_list):
+        print('\nThe centers of the clusters are located at: \n')
+        URL_prefix = 'http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\'
+        number = 1
+        markers = ''
+        for centroid in centroid_list:
+            if centroid.size > 1:
+                form = (number, centroid.center, centroid.size)
+                print('{} --> {} has {} points around it.'.format(*form))
+                if len(URL_prefix + markers) + 15 > 2000:
+                    print(''.join([URL_prefix, markers, '&sensor=false']))
+                    markers = ''
+                form = (number, centroid.center.lat, centroid.center.lon)
+                markers += '&markers=%7Ccolor:blue%7Clabel:{}%7C{},{}'.format(*form)
+            number += 1
 
-			if (len(URL_prefix +  markers) + 15 > 2000):
-				print(URL_prefix + markers + '&sensor=false')
-				markers = ''
-			form = (color, chr(letter), point[2], point[3])
-			markers += '&markers=%7Ccolor:{}%7Clabel:{}%7C{},{}'.format(*form)
-			letter += 1
+        final_URL = '{}{}%7C&sensor=false'.format(URL_prefix, markers)
+        print(
+            '\nThe map of the cluster centers\n{}\n{}\n'.format(final_URL, '_' * 37))
 
-		final_URL = '{}{}%7C&sensor=false'.format(URL_prefix, markers)
-		print(final_URL, end='\n\n')
+    def plotter(self, parsed_file, center):
+        if center.lat == 0 and center.lon == 0:
+            return 0
+        colors = ['black', 'purple', 'green',
+                  'red', 'orange', 'yellow', 'white']
+        URL_prefix = 'http://maps.googleapis.com/maps/api/staticmap?size=640x640&maptype=roadmap\\'
+        color = colors[5]
+        markers = '&markers=%7Ccolor:blue%7C{},{}'.format(
+            center.lat, center.lon)
 
-	def scan_clustering(self, placement):
-		'''Check that there is at least one point in each cluster'''
-		for i in range(len(placement)):
-			if not placement[i]:
-				return False
-		return True
+        letter = ord('A')
+        for point in parsed_file:
+            if color != colors[int(point[4]) % (len(colors) - 1)]:
+                color = colors[int(point[4]) - 1]
 
-	#Procedure stuff
-	def read_existing_data(self, file_name='cluster.csv'):
-		'''Pull in a headered csv and return the header and data'''
-		parsed_file = []
-		with open(file_name) as csvfile:
-			info_reader = csv.reader(csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
-			for row in info_reader:
-				column = []
-				for item in row:
-					column.append(item.strip())
-				parsed_file.append(column)
+            if (len(URL_prefix + markers) + 15 > 2000):
+                print(URL_prefix + markers + '&sensor=false')
+                markers = ''
+            form = (color, chr(letter), point[2], point[3])
+            markers += '&markers=%7Ccolor:{}%7Clabel:{}%7C{},{}'.format(*form)
+            letter += 1
 
-		header = parsed_file.pop(0)
-		return (header, parsed_file) 
+        final_URL = '{}{}%7C&sensor=false'.format(URL_prefix, markers)
+        print(final_URL, end='\n\n')
 
-	def write_data(self, data, file_name='cluster.csv'):
-		'''Write sorted data out'''
-		with open(file_name, 'w') as updated_file:
-			data.sort(key=lambda data: data[4])
-			writer = csv.writer(updated_file, delimiter='|', quoting=csv.QUOTE_NONE)
-			writer.write_row(header)
-			for row in data:
-				writer.write_row(row)
+    def scan_clustering(self, placement):
+        '''Check that there is at least one point in each cluster'''
+        for i in range(len(placement)):
+            if not placement[i]:
+                return False
+        return True
 
-	def create_observation_vector(self, parsed_file):
-		obs_vector = []
-		for i in range(len(parsed_file)):
-			item = parsed_file[i]
-			name = item[0]
-			address = item[1]
-			lat = item[2]
-			lon = item[3]    
-			if not lat or not lon:
-				temp = Geopoint(name, address)
-				item[2] = temp.lat
-				item[3] = temp.lon
-			else:
-				temp = Geopoint(name, address, lat, lon)
-			
-			obs_vector.append([temp.lon, temp.lat])
+    def read_existing_data(self, file_name='cluster.csv'):
+        '''Pull in a headered csv and return the header and data'''
+        parsed_file = []
+        with open(file_name) as csvfile:
+            info_reader = csv.reader(
+                csvfile, delimiter='|', quoting=csv.QUOTE_NONE)
+            for row in info_reader:
+                column = []
+                for item in row:
+                    column.append(item.strip())
+                parsed_file.append(column)
 
-		return obs_vector
+        self.header = parsed_file.pop(0)
+        return parsed_file
 
-	def perform_clustering(self, observation):
-		matrix = array(obs_vector)
-		last_solution = None
-		for i in range (2, len(obs_vector)//2):
-			cluster = kmean.kmeans2(matrix, i, iter=1000, minit='random')
-			if Clump.scan_clustering(cluster[1]):
-				last_solution = cluster
-				print('Clusters =', i, cluster[1])
-		 
-		if not last_solution:
-			print('Rerun the program, this sort of thing takes time.')
-			exit(1) #change this
-		return last_solution
+    def write_data(self, data, file_name='cluster.csv'):
+        '''Write sorted data out'''
+        with open(file_name, 'w') as updated_file:
+            data.sort(key=lambda data: data[4])
+            writer = csv.writer(updated_file, delimiter='|',
+                                quoting=csv.QUOTE_NONE)
+            writer.write_row(header)
+            for row in data:
+                writer.write_row(row)
 
-	def initialize_clusters(self, codedList):
+    def create_observation_vector(self, parsed_file):
+        obs_vector = []
+        for i in range(len(parsed_file)):
+            item = parsed_file[i]
+            name = item[0]
+            address = item[1]
+            lat = item[2]
+            lon = item[3]
+            if not lat or not lon:
+                temp = Geopoint(name, address)
+                item[2] = temp.lat
+                item[3] = temp.lon
+            else:
+                temp = Geopoint(name, address, lat, lon)
 
-		for i in range (0, numberOfClusters):
-			center.append(Cluster())
-		codedList = []
-		for i in range (len(parsed_file)):
-			print(last_solution)
-			cluster = last_solution[1][i]
-			parsed_file[i][4] = cluster
-			clusterNode = center[cluster]
-			theCenter = center[cluster].center
-			if (theCenter.lat == 0 and theCenter.lon == 0):
-				theCenter.lat = last_solution[0][cluster][1]
-				theCenter.lon = last_solution[0][cluster][0]
+            obs_vector.append([temp.lon, temp.lat])
 
-			clusterNode.points.append(parsed_file[i])
+        return obs_vector
 
-Clump.plot_center(center)
+    def perform_clustering(self, observation):
+        print(observation)
+        matrix = array(observation)
+        last_solution = None
+        for i in range(2, len(observation) // 2):
+            cluster = kmean.kmeans2(matrix, i, iter=1000, minit='random')
+            if self.scan_clustering(cluster[1]):
+                last_solution = cluster
+                print('Clusters =', i, cluster[1])
 
-cluster_number = 1
-for centroid in center:
-	form = (cluster_number, centroid.center, centroid.size)
-	print('Cluster {} centered at {} size: {}'.format(*form))
-	print('A static map of the cluster')
-	Clump.plotter(centroid.points, centroid.center)
-	i = ord('A')
-	pointsLined = 'from: Capitol Boise ID'
-	for points in centroid.points:
-		print('{} is {}, {}'.format(chr(i), points[0], points[1]))
-		pointsLined += (' to: ' + points[1])
-		i += 1
-	pointsLined += ' to: Capitol Boise ID'
-	print('\nPaste this into Google Maps for distance info:\n{}\n{}'.format(pointsLined, '_'*43))
-	cluster_number += 1
+        if not last_solution:
+            print('Rerun the program, this sort of thing takes time.')
+            exit(1)  # change this
+        print(last_solution)
+        return last_solution
+
+    def initialize_clusters(self, last_solution, parsed_file):
+
+        for i in range(0, max(last_solution[1])):
+            self.clusters.append(Cluster())
+        codedList = []
+        for i in range(len(parsed_file)):
+            cluster = last_solution[1][i]
+            parsed_file[i][4] = cluster
+            clusterNode = self.clusters[cluster]
+            theCenter = self.clusters[cluster].center
+            if (theCenter.lat == 0 and theCenter.lon == 0):
+                theCenter.lat = last_solution[0][cluster][1]
+                theCenter.lon = last_solution[0][cluster][0]
+
+            clusterNode.points.append(parsed_file[i])
+
+    def __str__(self):
+        cluster_number = 1
+        for centroid in self.clusters:
+            form = (cluster_number, centroid.center, centroid.size)
+            print('Cluster {} centered at {} size: {}'.format(*form))
+            print('A static map of the cluster')
+            Clump.plotter(centroid.points, centroid.center)
+            i = ord('A')
+            pointsLined = 'from: Capitol Boise ID'
+            for points in centroid.points:
+                print('{} is {}, {}'.format(chr(i), points[0], points[1]))
+                pointsLined += (' to: ' + points[1])
+                i += 1
+            pointsLined += ' to: Capitol Boise ID'
+            print('\nPaste this into Google Maps for distance info:\n{}\n{}'.format(
+                pointsLined, '_' * 43))
+            cluster_number += 1
+
+
+if __name__ == '__main__':
+    test = Clump()
+    data = test.read_existing_data()
+    obs_vector = test.create_observation_vector(data)
+    clustered = test.perform_clustering(obs_vector)
--- nexus.py
+++ nexus.py
@@ -1,78 +1,90 @@
 from math import atan2, cos, radians, sin, sqrt
+
 import requests
 
-class Cluster:
-	def __init__(self, center, points, letter='A'):
-		self.center = center
-		self.points = points
-		self.number = letter
 
-	def __str__(self):
-		point_list = '\n'.split([point for point in self.points])
-		form = (self.letter, self.center, point_list)
-		return 'Cluster {} is centered at {} with the following points associated:\n{}'.format(*form)
+class Cluster:
+
+    def __init__(self, center, points, letter='A'):
+        self.center = center
+        self.points = points
+        self.number = letter
+
+    def __str__(self):
+        point_list = '\n'.split([str(point) for point in self.points])
+        form = (self.letter, self.center, point_list)
+        return 'Cluster {} is centered at {} with the following points associated:\n{}'.format(*form)
+
 
 class Point:
-	"Has latitude and lonitude attributes"
-	def __init__(self, lat=0, lon=0, *args):
-		self.lat = lat
-		self.lon = lon
 
-	def distance_between(self, other_point):
-		'''Haversine function from http://www.movable-type.co.uk/scripts/gis-faq-5.1.html'''
-		dlat = radians(self.lat - other_point.lat)
-		dlon = radians(self.lon - other_point.lon)
+    "Has latitude and lonitude attributes"
 
-		R = 3963.1676  #Radius of the earth in miles
+    def __init__(self, lat=0, lon=0, *args):
+        self.lat = lat
+        self.lon = lon
 
-		a = sin(dlat/2)**2 + cos(radians(self.lat))*cos(radians(other_point.lat))*(sin(dlon/2)**2)
-		c = 2 * atan2(sqrt(a), sqrt(1 - a))
-		return R * c
+    def distance_between(self, other_point):
+        '''Haversine function from http://www.movable-type.co.uk/scripts/gis-faq-5.1.html'''
+        dlat = radians(self.lat - other_point.lat)
+        dlon = radians(self.lon - other_point.lon)
 
-	def is_uninitialized(self):
-		'''Make sure one of the coordinates is non-zero. There's only open
-		place it won't work...'''
-		return self.lat and self.lon
+        R = 3963.1676  # Radius of the earth in miles
 
-	def __str__(self):
-		return '({}, {})'.format(self.lat, self.lon)
+        a = sin(dlat / 2) ** 2 + cos(radians(self.lat)) * \
+            cos(radians(other_point.lat)) * (sin(dlon / 2) ** 2)
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return R * c
+
+    def is_uninitialized(self):
+        '''Make sure one of the coordinates is non-zero. There's only open
+        place it won't work...'''
+        return self.lat and self.lon
+
+    def __str__(self):
+        return '({}, {})'.format(self.lat, self.lon)
 
 
 class Geopoint(Point):
-	'''Geopoints have names and addresses as well as geocoded points'''
-	def __init__(self, name, address, lat=0, lon=0):
-		self.name = name
-		self.address = address.replace(',', '')
-		if self.is_uninitialized:
-			self.geocodeset()
 
-	def geocodeset(self):
-		'''Change default lat lon to actual address(hopefully)'''
-		if self.address:
-			plussed_address = self.address.replace(' ', '+')
-			new_point = self.get_lat_lon(plussed_address)
-			self.geocode = Point(*new_point)
+    '''Geopoints have names and addresses as well as geocoded points'''
 
-	def get_lat_lon(self, location):
-		output = 'json'
-		base_url = 'http://maps.google.com/maps/api/geocode/{}?address={}&sensor=false'
-		request = base_url.format(output, location)
-		raw_json = requests.get(request)
-		data = raw_json.json()
+    def __init__(self, name, address, lat=0, lon=0):
+        self.name = name
+        self.address = address.replace(',', '')
+        self.lat = lat
+        self.lon = lon
 
-		try:
-			location = data['results'][0]['geometry']['location']
-			return (location['lat'], location['lng'])
-		except Exception as e:
-			print('Something went wrong...{}'.format(e))
+        if self.is_uninitialized:
+            self.geocodeset()
 
-		return (0, 0)
+    def geocodeset(self):
+        '''Change default lat lon to actual address(hopefully)'''
+        if self.address:
+            plussed_address = self.address.replace(' ', '+')
+            new_point = self.get_lat_lon(plussed_address)
+            self.lat = new_point[0]
+            self.lon = new_point[1]
 
-	def __str__(self):
-		'''Override of the default. Returns a string representation of the 
-		Geopoint.'''
-		raw_rep = '{} {} {}'.format(self.name, self.address, self.geocode)
-		return raw_rep.title()
+    def get_lat_lon(self, location):
+        output = 'json'
+        base_url = 'http://maps.google.com/maps/api/geocode/{}?address={}&sensor=false'
+        request = base_url.format(output, location)
+        raw_json = requests.get(request)
+        data = raw_json.json()
+        print(data)
+
+        try:
+            location = data['results'][0]['geometry']['location']
+            return (location['lat'], location['lng'])
+        except Exception as e:
+            print('Something went wrong...{}'.format(e))
+
+        return (0, 0)
+
+    def __str__(self):
+        raw_rep = '{} {} {}'.format(self.name, self.address, self.geocode)
+        return raw_rep.title()
 
 if __name__ == '__main__':
-	print(Geopoint('Boise Capitol', 'Capitol Boise ID'))
+    print(Geopoint('Boise Capitol', 'Capitol Boise ID'))
